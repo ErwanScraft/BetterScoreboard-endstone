6f428dae210c1fea3c052d57263e1b1235d6fbff
ErwanScraft <erwansudandi@gmail.com>
2026-09-06 13:18:24 +0700

    feat: show scoreboard on player join

--- src/better_scoreboard/plugin.py
+++ src/better_scoreboard/plugin.py
@@ -1,5 +1,7 @@
 from .config import BetterScoreboardConfig
 from .manager import BetterScoreboardManager
+from endstone.event import event_handler
+from endstone.event.player import PlayerJoinEvent
 from endstone.plugin import Plugin
 
 
@@ -21,3 +23,7 @@ class BetterScoreboardPlugin(Plugin):
         self._scoreboard_manager.destroy()
     
         self.logger.info("BetterScoreboard disabled.")
+    
+    @event_handler
+    def on_player_join(self, event: PlayerJoinEvent) -> None:
+        self._scoreboard_manager.show(event.player)
